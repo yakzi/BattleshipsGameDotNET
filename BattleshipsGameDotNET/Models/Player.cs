d401b84acf03445f36186e23129fbf17a999d24f
yakzi <zieba.jakub.97@gmail.com>
2022-01-10 19:20:57 +0100

    ADD Player class, with required fields and props

---
+++ BattleshipsGameDotNET/Models/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace BattleshipsGameDotNET.Models
+{
+    public class Player
+    {
+        public int Score;
+        public List<Point> Hits = new List<Point>();
+        public List<Point> Board { get; set; }
+    }
+}
