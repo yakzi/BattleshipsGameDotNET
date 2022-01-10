f3620a32bc475fe8130ba9ce1946eafdff2dbe52
yakzi <zieba.jakub.97@gmail.com>
2022-01-10 19:23:47 +0100

    ADD CreateBoard method, for creating base, empty game board

--- BattleshipsGameDotNET/Controllers/GameController.cs
+++ BattleshipsGameDotNET/Controllers/GameController.cs
@@ -1,8 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BattleshipsGameDotNET.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BattleshipsGameDotNET.Controllers
 {
     public class GameController : Controller
     {
+        private void CreateBoard(Player player)
+        {
+            player.Board = new List<Point>();
+            for (var x = 0; x < 10; x++)
+            {
+                for (var y = 0; y < 10; y++)
+                {
+                    player.Board.Add(new Point
+                    {
+                        Field = Field.Empty,
+                        X = x,
+                        Y = y
+                    });
+                }
+            }
+        }
     }
 }
