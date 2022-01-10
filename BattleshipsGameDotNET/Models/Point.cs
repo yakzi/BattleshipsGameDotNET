e9fb9df40ac20e837180e59fb70ca2987b388d49
yakzi <zieba.jakub.97@gmail.com>
2022-01-10 19:19:37 +0100

    ADD Point class, which will keep coordinates and Field as props

---
+++ BattleshipsGameDotNET/Models/Point.cs
@@ -0,0 +1,9 @@
+﻿namespace BattleshipsGameDotNET.Models
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Field Field { get; set; }
+    }
+}
